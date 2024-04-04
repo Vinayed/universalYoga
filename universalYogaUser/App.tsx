def3ee43fb1e54a99d95f651a0a88b538c42bd4c
Vinayed <165804218+Vinayed@users.noreply.github.com>
2024-04-04 20:07:11 +0530

    Delete universalYogaUser directory

--- universalYogaUser/App.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Navigate from './src/navigate/NavigateIndex';
-import LoginScreen from './src/component/loginIn/LoginScreen';
-const Apps = () => {
-  return <Navigate />;
-};
-
-export default Apps;
